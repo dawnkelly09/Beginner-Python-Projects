1f2f52abd80be7642b2167d809e0625c18938cfe
Dawn Kelly <83190195+dawnkelly09@users.noreply.github.com>
2021-09-14 17:02:53 -0700

    Add files via upload

---
+++ first_madlib.py
@@ -0,0 +1,8 @@
+adj1 = input("Adjective: ")
+verb1 = input("Verb: ")
+fav_twit = input("Favorite Twitter user: ")
+
+madlib = f"Learning to code is really {adj1}. People think anyone can {verb1}\
+ it but, {fav_twit} knows better!"
+
+print(madlib)
